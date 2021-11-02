222b64d09f0fa0d83c8b0f09ea28265cb26458b0
Bosco Yung <user.email=15840328+bhky@users.noreply.github.com>
2021-11-02 06:38:44 +0100

    Add weights H5 file, removed npy file, clean up one-off code for loading weights from npy

--- opennsfw2/model.py
+++ opennsfw2/model.py
@@ -4,79 +4,15 @@ https://github.com/mdietrichstein/tensorflow-open_nsfw
 https://github.com/yahoo/open_nsfw
 """
 
-from typing import Dict, Tuple
+from typing import Optional, Tuple
 
-import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
-
-WEIGHTS: Dict[str, Dict[str, np.ndarray]] = np.load(
-    "../weights/open_nsfw-weights.npy", encoding="latin1", allow_pickle=True
-).item()
-
-
-def _get_weights(layer_name: str, field_name: str) -> np.ndarray:
-    if layer_name not in WEIGHTS:
-        raise ValueError(f"No weights found for layer {layer_name}.")
-
-    w = WEIGHTS[layer_name]
-    if field_name not in w:
-        raise ValueError(f"No field {field_name} in layer {layer_name}.")
-
-    return w[field_name].astype(np.float32)
-
-
-def _fully_connected(name: str, units: int) -> layers.Dense:
-    return layers.Dense(
-        name=name,
-        units=units,
-        kernel_initializer=tf.constant_initializer(
-            _get_weights(name, "weights")
-        ),
-        bias_initializer=tf.constant_initializer(
-            _get_weights(name, "biases")
-        )
-    )
-
-
-def _conv2d(
-        name: str,
-        num_filters: int,
-        kernel_size: int,
-        stride: int,
-        padding: str = "same"
-) -> layers.Conv2D:
-    return layers.Conv2D(
-        name=name,
-        filters=num_filters,
-        kernel_size=kernel_size,
-        strides=stride,
-        padding=padding,
-        kernel_initializer=tf.constant_initializer(
-            _get_weights(name, "weights")
-        ),
-        bias_initializer=tf.constant_initializer(
-            _get_weights(name, "biases")
-        )
-    )
 
 
 def _batch_norm(name: str) -> layers.BatchNormalization:
     return layers.BatchNormalization(
-        name=name,
-        epsilon=1e-05,  # Default used in Caffe.
-        gamma_initializer=tf.constant_initializer(
-            _get_weights(name, "scale")
-        ),
-        beta_initializer=tf.constant_initializer(
-            _get_weights(name, "offset")
-        ),
-        moving_mean_initializer=tf.constant_initializer(
-            _get_weights(name, "mean")
-        ),
-        moving_variance_initializer=tf.constant_initializer(
-            _get_weights(name, "variance")
-        ),
+        name=name, epsilon=1e-05,  # Default used in Caffe.
     )
 
 
@@ -94,41 +30,41 @@ def _conv_block(
     bn_name_base = f"bn_stage{stage}_block{block}_branch"
     shortcut_name_post = f"_stage{stage}_block{block}_proj_shortcut"
 
-    shortcut = _conv2d(
+    shortcut = layers.Conv2D(
         name=f"conv{shortcut_name_post}",
-        num_filters=num_filters_3,
+        filters=num_filters_3,
         kernel_size=1,
-        stride=stride,
+        strides=stride,
         padding="same"
     )(inputs)
 
     shortcut = _batch_norm(f"bn{shortcut_name_post}")(shortcut)
 
-    x = _conv2d(
+    x = layers.Conv2D(
         name=f"{conv_name_base}2a",
-        num_filters=num_filters_1,
+        filters=num_filters_1,
         kernel_size=1,
-        stride=stride,
+        strides=stride,
         padding="same"
     )(inputs)
     x = _batch_norm(f"{bn_name_base}2a")(x)
     x = tf.nn.relu(x)
 
-    x = _conv2d(
+    x = layers.Conv2D(
         name=f"{conv_name_base}2b",
-        num_filters=num_filters_2,
+        filters=num_filters_2,
         kernel_size=kernel_size,
-        stride=1,
+        strides=1,
         padding="same"
     )(x)
     x = _batch_norm(f"{bn_name_base}2b")(x)
     x = tf.nn.relu(x)
 
-    x = _conv2d(
+    x = layers.Conv2D(
         name=f"{conv_name_base}2c",
-        num_filters=num_filters_3,
+        filters=num_filters_3,
         kernel_size=1,
-        stride=1,
+        strides=1,
         padding="same"
     )(x)
     x = _batch_norm(f"{bn_name_base}2c")(x)
@@ -150,31 +86,31 @@ def _identity_block(
     conv_name_base = f"conv_stage{stage}_block{block}_branch"
     bn_name_base = f"bn_stage{stage}_block{block}_branch"
 
-    x = _conv2d(
+    x = layers.Conv2D(
         name=f"{conv_name_base}2a",
-        num_filters=num_filters_1,
+        filters=num_filters_1,
         kernel_size=1,
-        stride=1,
+        strides=1,
         padding="same"
     )(inputs)
     x = _batch_norm(f"{bn_name_base}2a")(x)
     x = tf.nn.relu(x)
 
-    x = _conv2d(
+    x = layers.Conv2D(
         name=f"{conv_name_base}2b",
-        num_filters=num_filters_2,
+        filters=num_filters_2,
         kernel_size=kernel_size,
-        stride=1,
+        strides=1,
         padding="same"
     )(x)
     x = _batch_norm(f"{bn_name_base}2b")(x)
     x = tf.nn.relu(x)
 
-    x = _conv2d(
+    x = layers.Conv2D(
         name=f"{conv_name_base}2c",
-        num_filters=num_filters_3,
+        filters=num_filters_3,
         kernel_size=1,
-        stride=1,
+        strides=1,
         padding="same"
     )(x)
     x = _batch_norm(f"{bn_name_base}2c")(x)
@@ -184,15 +120,16 @@ def _identity_block(
     return tf.nn.relu(x)
 
 
-def make_open_nsfw_model_and_load_weights(
-        input_shape: Tuple[int, int, int] = (224, 224, 3)
+def make_open_nsfw_model(
+        input_shape: Tuple[int, int, int] = (224, 224, 3),
+        weights_path: Optional[str] = "../weights/open_nsfw_weights.h5"
 ) -> tf.keras.Model:
     image_input = layers.Input(shape=input_shape, name="input")
     x = image_input
 
     x = tf.pad(x, [[0, 0], [3, 3], [3, 3], [0, 0]], "CONSTANT")
-    x = _conv2d("conv_1", num_filters=64, kernel_size=7, stride=2,
-                padding="valid")(x)
+    x = layers.Conv2D(name="conv_1", filters=64, kernel_size=7, strides=2,
+                      padding="valid")(x)
 
     x = _batch_norm("bn_1")(x)
     x = tf.nn.relu(x)
@@ -247,16 +184,17 @@ def make_open_nsfw_model_and_load_weights(
 
     x = layers.Reshape((-1, 1024))(x)
 
-    logits = _fully_connected(name="fc_nsfw", units=2)(x)
+    logits = layers.Dense(name="fc_nsfw", units=2)(x)
     output = tf.nn.softmax(logits, name="predictions")
 
     model = tf.keras.Model(image_input, output)
+    if weights_path is not None:
+        model.load_weights(weights_path)
     return model
 
 
 def main() -> None:
-    model = make_open_nsfw_model_and_load_weights()
-    model.save_weights("../weights/open_nsfw_weights.h5", save_format="h5")
+    model = make_open_nsfw_model()
 
 
 if __name__ == "__main__":
