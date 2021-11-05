1fa943d57860a5a6be870c3229718a32794885f2
Bosco Yung <user.email=15840328+bhky@users.noreply.github.com>
2021-11-05 15:32:01 +0800

    Add install and test scripts

---
+++ tests/install_from_local.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 -m pip install .
---
+++ tests/install_from_local_and_test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+./tests/install_from_local.sh
+./tests/run_code_checks.sh
