8e62e0d33a7689dbc1df3855aff15a5f2245adf7
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-11-13 17:21:20 +0800

    Rename test functions

--- tests/_test.py
+++ tests/_test.py
@@ -24,14 +24,14 @@ class TestModel(unittest.TestCase):
         for expected_score, predicted_score in zip(expected, predicted):
             self.assertAlmostEqual(expected_score, predicted_score, places=3)
 
-    def test_inference_yahoo_preprocessing(self) -> None:
+    def test_predict_images_yahoo_preprocessing(self) -> None:
         expected_scores = np.array([0.012, 0.756, 0.067])
         predicted_scores = n2.predict(
             IMAGE_PATHS, preprocessing=n2.Preprocessing.YAHOO
         )[:, 1]
         self._assert(expected_scores, predicted_scores)
 
-    def test_inference_simple_preprocessing(self) -> None:
+    def test_predict_images_simple_preprocessing(self) -> None:
         expected_scores = np.array([0.001, 0.597, 0.003])
         predicted_scores = n2.predict(
             IMAGE_PATHS, preprocessing=n2.Preprocessing.SIMPLE
