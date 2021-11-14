70abd10f3bffd01385861bfc3d8a2f3dc358c6af
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-11-14 09:08:23 +0800

    pylint --errors-only

--- tests/run_code_checks.sh
+++ tests/run_code_checks.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+find opennsfw2 -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --errors-only
 find opennsfw2 -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --exit-zero
 find opennsfw2 -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs --implicit-reexport
 find tests -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 python3 -m unittest
