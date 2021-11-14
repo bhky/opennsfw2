d39bfcb7fb0d102ad5c65f0953983df9a9854bdf
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-11-14 15:14:14 +0800

    Update test

--- tests/_test.py
+++ tests/_test.py
@@ -4,8 +4,7 @@ Unit tests.
 
 import os
 import unittest
-
-import numpy as np
+from typing import List
 
 import opennsfw2 as n2
 
@@ -21,23 +20,28 @@ VIDEO_PATH = os.path.join(BASE_DIR, "test_video.mp4")
 
 class TestModel(unittest.TestCase):
 
-    def _assert(self, expected: np.ndarray, predicted: np.ndarray) -> None:
+    def _assert(self, expected: List[float], predicted: List[float]) -> None:
         for expected_score, predicted_score in zip(expected, predicted):
             self.assertAlmostEqual(expected_score, predicted_score, places=3)
 
     def test_predict_images_yahoo_preprocessing(self) -> None:
-        expected_scores = np.array([0.012, 0.756, 0.067])
+        expected_scores = [0.012, 0.756, 0.067]
         predicted_scores = n2.predict_images(
             IMAGE_PATHS, preprocessing=n2.Preprocessing.YAHOO
-        )[:, 1]
+        )
         self._assert(expected_scores, predicted_scores)
 
     def test_predict_images_simple_preprocessing(self) -> None:
-        expected_scores = np.array([0.001, 0.597, 0.003])
+        expected_scores = [0.001, 0.597, 0.003]
         predicted_scores = n2.predict_images(
             IMAGE_PATHS, preprocessing=n2.Preprocessing.SIMPLE
-        )[:, 1]
+        )
         self._assert(expected_scores, predicted_scores)
+
+    def test_predict_image(self) -> None:
+        self.assertAlmostEqual(
+            0.756, n2.predict_image(IMAGE_PATHS[1]), places=3
+        )
 
     def test_predict_video_frames(self) -> None:
         elapsed_seconds, nsfw_probabilities = n2.predict_video_frames(
