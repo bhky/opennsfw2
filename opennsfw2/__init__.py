11c5ed53e5404c67e8c4bcaf9d95bda949ee4c2f
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-11-17 15:32:51 +0800

    Add mypy doc references regarding the re-export

--- opennsfw2/__init__.py
+++ opennsfw2/__init__.py
@@ -1,3 +1,4 @@
+# See: https://mypy.readthedocs.io/en/stable/command_line.html#cmdoption-mypy-no-implicit-reexport
 from ._image import Preprocessing as Preprocessing
 from ._image import preprocess_image as preprocess_image
 from ._inference import predict_image as predict_image
