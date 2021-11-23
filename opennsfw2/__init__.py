e31296fbcfb2a6113a2b870bebf4bf681a7b9491
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-11-23 08:52:04 +0800

    Version 0.6.0

--- opennsfw2/__init__.py
+++ opennsfw2/__init__.py
@@ -6,6 +6,6 @@ from ._inference import predict_images as predict_images
 from ._inference import predict_video_frames as predict_video_frames
 from ._model import make_open_nsfw_model as make_open_nsfw_model
 
-__version__ = "0.5.3"
+__version__ = "0.6.0"
 __author__ = "Bosco Yung"
 __license__ = "MIT"
